37d4997757486e7942079c882a96ea68b834e1ee
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2022-02-24 13:56:23 -0300

    Adicionado docker em script 03-ferramentas_diversas

--- 03-ferramentas_diversas.sh
+++ 03-ferramentas_diversas.sh
@@ -4,8 +4,10 @@ BASE=$(dirname $0)
 source "$BASE/log.sh"
 
 function instalar-ferramentas {
-    sudo apt install -y lnav htop nmon nmap
-    sudo apt install -y virtualbox virtualbox-qt
+    apty="sudo apt install -y"
+    $apty lnav htop nmon nmap
+    $apty virtualbox virtualbox-qt
+    $apty docker docker-compose docker-doc
 }
 
 instalar-ferramentas | log $0
