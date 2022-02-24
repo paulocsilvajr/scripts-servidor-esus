df5fb3a23615f8a2f11b60d1f15cd6b5b5d5eb56
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2022-02-24 16:40:29 -0300

    Adicionado script para instalação e configuração de SSHd

---
+++ 04-SSHd.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+BASE=$(dirname $0)
+source "$BASE/log.sh"
+
+function instalar-sshd {
+    sudo apt install -y openssh-server
+}
+
+function configurar-sshd {
+    echo "Adicionar usuário 'ti' para uso em acesso remoto via SSH"
+    sudo useradd --system --no-create-home ti
+
+    NOVACONFIG="AllowUsers ti\nDenyUsers root esus"
+    CONFIGSSH="/etc/ssh/sshd_config"
+    COPIACONFIGSSH="${CONFIGSSH}.original"
+
+    echo -e "\nGerar cópia de arquivo de configuração de SSH($CONFIGSSH), adicionar permissão para usuário 'ti' e remover permissão de usuários 'root' e 'esus'"
+    ! test -f $COPIACONFIGSSH &&
+        sudo cp -v $CONFIGSSH $COPIACONFIGSSH &&
+        sudo echo -e $NOVACONFIG >> $CONFIGSSH
+
+    echo -e "\nAdicionar excessão para SSH em UFW"
+    sudo ufw allow ssh
+}
+
+instalar-sshd | log $0
+configurar-sshd | log $0
