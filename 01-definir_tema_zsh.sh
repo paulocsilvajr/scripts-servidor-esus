9cf0bc8809f548a7b14a67eb3f58166ea86d445e
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2022-02-24 10:33:48 -0300

    Removido script para definir tema e adicionado mensagens de configuração de zsh/oh my zsh em script 01-oh_my_zsh

--- 01-definir_tema_zsh.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env zsh
-
-omz theme set bureau
--- 01-oh_my_zsh.sh
+++ 01-oh_my_zsh.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
-sudo apt install zsh curl wget
+function instalar-requisitos-zsh {
+    sudo apt install zsh curl wget python3-pygments chroma
+}
 
+function instalar-oh-my-zsh {
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    echo "Para listar temas:"
+    echo -e "omz theme list\n"
+    echo "Para definir um tema(bureau):"
+    echo -e "omz theme set bureau\n"
+    echo "Para ativar plugins(git asdf python pip colored-man-pages colorize docker docker-compose ufw):"
+    echo -e "omz plugin enable git asdf python pip colored-man-pages colorize docker docker-compose ufw\n"
+}
+
+instalar-requisitos-zsh
+instalar-oh-my-zsh
