9d96b4bb74ea8119c29b10f8f3847ca8d8d57729
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2022-02-25 09:27:48 -0300

    Adicionado passwd para definir senha em script 04...

--- 04-SSHd.sh
+++ 04-SSHd.sh
@@ -11,11 +11,15 @@ function configurar-sshd {
     echo "Adicionar usuário 'ti' para uso em acesso remoto via SSH"
     sudo useradd --system --no-create-home ti
 
+    echo -e "\nDefinir a senha informada para o usuário 'ti'"
+    echo $1 | passwd ti --stdin
+
     NOVACONFIG="AllowUsers ti\nDenyUsers root esus"
     CONFIGSSH="/etc/ssh/sshd_config"
     COPIACONFIGSSH="${CONFIGSSH}.original"
 
-    echo -e "\nGerar cópia de arquivo de configuração de SSH($CONFIGSSH), adicionar permissão para usuário 'ti' e remover permissão de usuários 'root' e 'esus'"
+    echo -e "\nGerar cópia de arquivo de configuração de SSH($CONFIGSSH), adicionar permissão para usuário 'ti' e remover permissão de usuários 'root' e 'esus', se necessário"
+    # somente será gerado cópia e adicionado nova configuração se não foi ainda gerado o backup .original com config original de sshd_config
     ! sudo test -f $COPIACONFIGSSH &&
         sudo cp -v $CONFIGSSH $COPIACONFIGSSH &&
         echo -e $NOVACONFIG | sudo tee -a $CONFIGSSH
@@ -27,5 +31,10 @@ function configurar-sshd {
     sudo ufw allow ssh
 }
 
+if ! [ -n $1 ]; then
+    echo "Informe uma senha para o usuário 'ti'" | log $0
+    exit 1
+fi
+
 instalar-sshd | log $0
-configurar-sshd | log $0
+configurar-sshd $1 | log $0
